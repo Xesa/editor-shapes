25f075330488cf95c740dbd0a5b7ab700cb57e5a
Xesa <fr.chesa@gmail.com>
2026-08-23 16:48:02 +0200

    feat(rectangle-zone): Revamped behaviour of rectangle zone to have more configuration options and being able to be masked.

--- addons/xesa/EditorShapes/classes/rectangle_zone_2d.gd
+++ addons/xesa/EditorShapes/classes/rectangle_zone_2d.gd
@@ -1,48 +1,154 @@
-## A rectangular zone defined by a [RectangleShape2D].
+## A rectangular zone that can be used for drawing shapes or
+## defining logical areas that are not tied to the physics system.
 ##
-## The shape of this node is only visible in the editor. If you want to
-## create a visible shape during runtime, use [RectangleColorZone2D] instead.[br]
+## The shape of this node is only visible in the editor by default.
+## If you want to make this shape visible during runtime, add a resource
+## to the [member zone_configuration] property and set the
+## [member RectangleZone2DResource.visible_on_runtime] property from the resource to true.[br][br]
 ##
-## This node provides custom editor visualization and handles for resizing the zone.
-## Supports per-zone snapping and configurable editor visibility.
+## Editor color, handle size, grid snapping and other configurations can also
+## be set with a custom resource. If no resource is provided, the default settings will be applied.
 @tool
 @icon("../icons/rectangle_zone_2d.svg")
 class_name RectangleZone2D extends Node2D
 
+const DEFAULT_EDITOR_COLOR := Color(0.35, 1.0, 0.35, 1.0)
 
-@export_group("Shape")
+## The size of the shape.
+@export var size := Vector2(16.0, 16.0)
 
-## The rectangle shape used by this zone.
-@export var shape: RectangleShape2D
-
-## Enables snapping when resizing the zone in the editor.
-@export var snap := true
-
-## The size of each snapping step when resizing the zone.
-@export var snap_size := Vector2(16.0, 16.0):
-	set(value):
-		snap_size = value.max(Vector2.ZERO)
+## Use a resource to customize this zone's settings. If you don't
+## use a resource, the default settings will be applied.
+@export var zone_configuration : RectangleZone2DResource
 
 
-@export_group("Visibility")
+var masking_zones : Array[RectangleMaskingZone2D] = []
+var geometry: Array[Rect2] = []
 
-## Keeps the rectangle visible in the editor even when this node is not selected.
-## Note: at least one [code]RectangleZone2D[/code] node has to be selected.
-@export var always_visible := true
 
-## The width of the rectangle outline in pixels.
-@export_range(1, 10, 1, "prefer_slider", "suffix:px")
-var line_size: int = 2
+# region Getter Methods
 
-## The size of the resize handles in pixels.
-@export_range(2, 20, 1, "prefer_slider", "suffix:px")
-var handle_size: int = 8
+func get_color() -> Color:
+	return zone_configuration.color if zone_configuration else Color.WHITE
 
-## The color and opacity of the rectangle outline. This only affects the editor.
-@export var line_color := Color(0.35, 1.0, 0.35, 0.25)
+func is_snap_enabled() -> bool:
+	return zone_configuration.snap if zone_configuration else true
 
-## The color and opacity used to fill the rectangle. This only affects the editor.
-@export var fill_color := Color(0.35, 1.0, 0.35, 0.0)
+func get_snap_size() -> Vector2:
+	return zone_configuration.snap_size if zone_configuration else Vector2(16.0, 16.0)
 
-## The color of the resize handles. This only affects the editor.
-@export var handle_color := Color(0.35, 1.0, 0.35, 1.0)
+func is_visible_on_runtime() -> bool:
+	return zone_configuration.visible_on_runtime if zone_configuration else false
+
+func replaces_editor_color() -> bool:
+	return zone_configuration.replace_fill_color if zone_configuration else false
+
+func is_always_visible() -> bool:
+	return zone_configuration.always_visible if zone_configuration else false
+
+func get_line_size() -> float:
+	return zone_configuration.line_size if zone_configuration else 1.0
+
+func get_handle_size() -> float:
+	return zone_configuration.handle_size if zone_configuration else 8.0
+	
+func get_editor_color() -> Color:
+	return zone_configuration.editor_color if zone_configuration else DEFAULT_EDITOR_COLOR
+
+func get_fill_color() -> Color:
+	
+	if replaces_editor_color():
+		return get_color()
+		
+	var color := get_editor_color()
+	var opacity := zone_configuration.fill_opacity if zone_configuration else 0.0
+	color.a = opacity / 100.0
+	return color
+		
+func get_global_rect() -> Rect2:
+	return Rect2(global_position - (size / 2.0), size)
+	
+func get_local_rect() -> Rect2:
+	return Rect2(-size / 2.0, size)
+	
+	
+# endregion
+
+
+# region Setter Methods
+
+func add_mask(mask : RectangleMaskingZone2D) -> void:
+	masking_zones.append(mask)
+	_build_geometry()
+
+# endregion
+
+
+# region Draw Methods
+
+
+func _draw() -> void:
+	
+	if is_visible_on_runtime():
+		
+		if !Engine.is_editor_hint() or replaces_editor_color():
+			
+			if geometry.size() == 0:
+				draw_rect(Rect2(-size / 2.0, size), get_color())
+				
+			else:
+				for rect in geometry:
+					draw_rect(rect, get_color())
+					
+					
+func _build_geometry() -> void:
+	var zone_rect := get_local_rect()
+	geometry = [zone_rect]
+
+	for mask in masking_zones:
+		var next: Array[Rect2] = []
+		var mask_position := to_local(mask.global_position)
+		var mask_rect := Rect2(mask_position - mask.size / 2.0, mask.size)
+
+		for rect in geometry:
+			next.append_array(_subtract_rect(rect, mask_rect))
+
+		geometry = next
+			
+			
+func _subtract_rect(source: Rect2, cutout: Rect2) -> Array[Rect2]:
+	var result: Array[Rect2] = []
+
+	if not source.intersects(cutout):
+		result.append(source)
+		return result
+
+	var intersection := source.intersection(cutout)
+
+	if intersection.position.y > source.position.y:
+		result.append(Rect2(
+			source.position,
+			Vector2(source.size.x, intersection.position.y - source.position.y)
+		))
+
+	if intersection.end.y < source.end.y:
+		result.append(Rect2(
+			Vector2(source.position.x, intersection.end.y),
+			Vector2(source.size.x, source.end.y - intersection.end.y)
+		))
+
+	if intersection.position.x > source.position.x:
+		result.append(Rect2(
+			Vector2(source.position.x, intersection.position.y),
+			Vector2(intersection.position.x - source.position.x, intersection.size.y)
+		))
+
+	if intersection.end.x < source.end.x:
+		result.append(Rect2(
+			Vector2(intersection.end.x, intersection.position.y),
+			Vector2(source.end.x - intersection.end.x, intersection.size.y)
+		))
+
+	return result
+	
+# endregion
--- addons/xesa/EditorShapes/editor_plugin/shape_editor.gd
+++ addons/xesa/EditorShapes/editor_plugin/shape_editor.gd
@@ -1,7 +1,17 @@
 @tool
 extends EditorPlugin
 
+# region Selected Node Info
+
+var selected_parent : Node2D
 var selected: RectangleZone2D
+var snap_enabled : bool
+var snap_size : Vector2
+
+# endregion
+
+
+# region Drag Info
 
 var dragging := false
 var drag_handle := -1
@@ -10,17 +20,50 @@ var drag_start_mouse := Vector2.ZERO
 var drag_start_local_mouse := Vector2.ZERO
 var drag_start_size := Vector2.ZERO
 var drag_start_position := Vector2.ZERO
+var drag_start_transform := Transform2D.IDENTITY
 var drag_snapped_size := Vector2.ZERO
+
+# endregion
 
 
 # region Editor Methods
 
-func _handles(object: Object) -> bool:
-	return object is RectangleZone2D
+func _handles(object : Object) -> bool:
+	
+	if object is RectangleZone2D:
+		return true
+	
+	if object is Node2D:
+		return _scan_children(object)
+			
+	return false
+	
+	
+func _scan_children(object : Object) -> bool:
+	
+	for child in object.get_children():
+		if child is RectangleZone2D:
+			return true
+			
+		if _scan_children(child) == true:
+			return true
+			
+	return false
 
 
-func _edit(object: Object) -> void:
-	selected = object as RectangleZone2D
+func _edit(object : Object) -> void:
+	
+	if object is RectangleZone2D:
+		selected = object as RectangleZone2D
+		selected_parent = null
+		
+	elif object is Node2D:
+		selected = null
+		selected_parent = object
+	
+	if selected:
+		snap_enabled = selected.is_snap_enabled()
+		snap_size = selected.get_snap_size()
 
 	dragging = false
 	drag_handle = -1
@@ -29,8 +72,9 @@ func _edit(object: Object) -> void:
 	drag_start_local_mouse = Vector2.ZERO
 	drag_start_size = Vector2.ZERO
 	drag_start_position = Vector2.ZERO
+	drag_start_transform = Transform2D.IDENTITY
 	drag_snapped_size = Vector2.ZERO
-	
+
 	update_overlays()
 	
 # endregion
@@ -38,30 +82,34 @@ func _edit(object: Object) -> void:
 
 # region Canvas Methods
 
-func _forward_canvas_draw_over_viewport(overlay: Control) -> void:
+func _forward_canvas_draw_over_viewport(overlay : Control) -> void:
 
 	var root := get_editor_interface().get_edited_scene_root()
-
+	
 	if root == null:
 		return
 
 	for node in root.find_children("*", "RectangleZone2D", true, false):
-		if node.shape and node.always_visible:
+		if node.is_always_visible():
 			_draw_rectangle(overlay, node)
+		
+	if selected_parent:
+		for node in selected_parent.find_children("*", "RectangleZone2D", true, false):
+			if !node.is_always_visible():
+				_draw_rectangle(overlay, node)
 
-	if selected and selected.shape:
-		if !selected.always_visible:
+	if selected:
+		if !selected.is_always_visible():
 			_draw_rectangle(overlay, selected)
 			
 		_draw_handles(overlay, selected)
 		
 		
-func _draw_rectangle(overlay: Control, zone: RectangleZone2D) -> void:
-	if zone.shape == null:
-		return
+func _draw_rectangle(overlay : Control, zone : RectangleZone2D) -> void:
+	
 
 	var transform := EditorInterface.get_editor_viewport_2d().get_final_transform()
-	var size := zone.shape.size
+	var size := zone.size
 
 	var local_points := PackedVector2Array([
 		Vector2(-size.x, -size.y) / 2.0,
@@ -75,20 +123,25 @@ func _draw_rectangle(overlay: Control, zone: RectangleZone2D) -> void:
 	for point in local_points:
 		points.append(transform * zone.to_global(point))
 
-	if zone.fill_color.a > 0.0:
-		overlay.draw_colored_polygon(points, zone.fill_color)
+	if !(zone.is_visible_on_runtime() and zone.replaces_editor_color()):
+		
+		var fill_color := zone.get_fill_color()
+		
+		if fill_color.a > 0.0:
+			overlay.draw_colored_polygon(points, fill_color)
 
-	overlay.draw_polyline(points, zone.line_color, zone.line_size)
-	overlay.draw_line(points[3], points[0], zone.line_color, zone.line_size)
+	overlay.draw_polyline(points, zone.get_editor_color(), zone.get_line_size())
+	overlay.draw_line(points[3], points[0], zone.get_editor_color(), zone.get_line_size())
 	
 	
-func _draw_handles(overlay: Control, zone: RectangleZone2D) -> void:
+func _draw_handles(overlay : Control, zone : RectangleZone2D) -> void:
+	
 	var handles := _get_handle_positions()
 
 	for point in handles:
 		overlay.draw_rect(
-			Rect2(point - Vector2(4, 4), zone.handle_size * Vector2.ONE),
-			zone.handle_color
+			Rect2(point - Vector2(4, 4), zone.get_handle_size() * Vector2.ONE),
+			zone.get_editor_color()
 		)
 		
 # endregion
@@ -96,8 +149,8 @@ func _draw_handles(overlay: Control, zone: RectangleZone2D) -> void:
 
 # region Input Methods
 
-func _forward_canvas_gui_input(event: InputEvent) -> bool:
-	if selected == null or selected.shape == null:
+func _forward_canvas_gui_input(event : InputEvent) -> bool:
+	if selected == null:
 		return false
 
 	if event is InputEventMouseButton:
@@ -112,18 +165,16 @@ func _forward_canvas_gui_input(event: InputEvent) -> bool:
 					dragging = true
 					drag_handle = i
 					drag_start_mouse = event.position
-					drag_start_size = selected.shape.size
+					drag_start_size = selected.size
 					drag_start_position = selected.position
+					drag_start_transform = selected.global_transform
 
 					var transform := EditorInterface.get_editor_viewport_2d().get_final_transform()
 					var inverse := transform.affine_inverse()
 					var global_mouse : Vector2 = inverse * event.position
 
 					drag_start_local_mouse = selected.to_local(global_mouse)
-					drag_snapped_size = _snap_size_to_grid(
-						drag_start_size,
-						selected.snap_size
-					)
+					drag_snapped_size = _snap_size_to_grid(drag_start_size)
 
 					return true
 
@@ -154,9 +205,9 @@ func _manage_undo_redo() -> void:
 				undo_redo.create_action("Resize RectangleZone2D")
 
 				undo_redo.add_do_property(
-					selected.shape,
+					selected,
 					"size",
-					selected.shape.size
+					selected.size
 				)
 
 				undo_redo.add_do_property(
@@ -166,7 +217,7 @@ func _manage_undo_redo() -> void:
 				)
 
 				undo_redo.add_undo_property(
-					selected.shape,
+					selected,
 					"size",
 					drag_start_size
 				)
@@ -183,28 +234,19 @@ func _manage_undo_redo() -> void:
 		
 		
 # region Snapping Methods
-		
-func _is_grid_snap_enabled() -> bool:
-	return selected.snap
 
+func _snap_point(point : Vector2) -> Vector2:
 
-func _get_grid_step() -> Vector2:
-	return selected.snap_size
+	if not is_zero_approx(snap_size.x):
+		point.x = round(point.x / snap_size.x) * snap_size.x
 
-
-func _snap_point(point: Vector2) -> Vector2:
-	var step := selected.snap_size
-
-	if not is_zero_approx(step.x):
-		point.x = round(point.x / step.x) * step.x
-
-	if not is_zero_approx(step.y):
-		point.y = round(point.y / step.y) * step.y
+	if not is_zero_approx(snap_size.y):
+		point.y = round(point.y / snap_size.y) * snap_size.y
 
 	return point
 	
 	
-func _snap_size(size: Vector2, snap_size: Vector2) -> Vector2:
+func _snap_size(size : Vector2) -> Vector2:
 	var result := drag_snapped_size
 
 	if not is_zero_approx(snap_size.x):
@@ -225,9 +267,9 @@ func _snap_size(size: Vector2, snap_size: Vector2) -> Vector2:
 	
 	
 func _snap_axis(
-	value: float,
-	current: float,
-	step: float
+	value : float,
+	current : float,
+	step : float
 ) -> float:
 	var hysteresis := step * 0.25
 
@@ -243,7 +285,7 @@ func _snap_axis(
 	return current
 	
 	
-func _snap_size_to_grid(size: Vector2, snap_size: Vector2) -> Vector2:
+func _snap_size_to_grid(size : Vector2) -> Vector2:
 	var result := size
 
 	if not is_zero_approx(snap_size.x):
@@ -261,13 +303,20 @@ func _snap_size_to_grid(size: Vector2, snap_size: Vector2) -> Vector2:
 
 func _get_handle_positions() -> PackedVector2Array:
 	var transform := EditorInterface.get_editor_viewport_2d().get_final_transform()
-	var size := selected.shape.size
+	var size := selected.size
 
 	var local_points := PackedVector2Array([
+		# Corners
 		Vector2(-size.x, -size.y) / 2.0,
 		Vector2( size.x, -size.y) / 2.0,
 		Vector2( size.x,  size.y) / 2.0,
 		Vector2(-size.x,  size.y) / 2.0,
+
+		# Center of lines
+		Vector2(0.0, -size.y / 2.0),
+		Vector2(size.x / 2.0, 0.0),
+		Vector2(0.0, size.y / 2.0),
+		Vector2(-size.x / 2.0, 0.0),
 	])
 
 	var points := PackedVector2Array()
@@ -278,16 +327,14 @@ func _get_handle_positions() -> PackedVector2Array:
 	return points
 
 
-func _resize_from_handle(mouse_position: Vector2, centered : bool) -> void:
-
+func _resize_from_handle(mouse_position : Vector2, centered : bool) -> void:
 	var transform := EditorInterface.get_editor_viewport_2d().get_final_transform()
 	var inverse := transform.affine_inverse()
 
 	var global_mouse := inverse * mouse_position
-	var local_mouse := selected.to_local(global_mouse)
+	var local_mouse := drag_start_transform.affine_inverse() * global_mouse
 
 	var mouse_delta := local_mouse - drag_start_local_mouse
-
 	var half := drag_start_size / 2.0
 
 	var start_handle := Vector2.ZERO
@@ -310,31 +357,70 @@ func _resize_from_handle(mouse_position: Vector2, centered : bool) -> void:
 			start_handle = Vector2(-half.x, half.y)
 			fixed_point = Vector2(half.x, -half.y)
 
+		4:
+			start_handle = Vector2(0.0, -half.y)
+			fixed_point = Vector2(0.0, half.y)
+
+		5:
+			start_handle = Vector2(half.x, 0.0)
+			fixed_point = Vector2(-half.x, 0.0)
+
+		6:
+			start_handle = Vector2(0.0, half.y)
+			fixed_point = Vector2(0.0, -half.y)
+
+		7:
+			start_handle = Vector2(-half.x, 0.0)
+			fixed_point = Vector2(half.x, 0.0)
+
 	var new_handle := start_handle + mouse_delta
+	var new_size := drag_start_size
 
-	var new_size := (new_handle - fixed_point).abs()
+	if drag_handle >= 4:
+		match drag_handle:
+			4, 6:
+				new_size.y = abs(new_handle.y - fixed_point.y)
+			5, 7:
+				new_size.x = abs(new_handle.x - fixed_point.x)
+	else:
+		new_size = (new_handle - fixed_point).abs()
 
-	if selected.snap:
-		new_size = _snap_size(new_size, selected.snap_size)
+	if snap_enabled:
+		new_size = _snap_size(new_size)
 		drag_snapped_size = new_size
 
 	new_size.x = max(new_size.x, 1.0)
 	new_size.y = max(new_size.y, 1.0)
 
-	var direction := Vector2(
-		sign(new_handle.x - fixed_point.x),
-		sign(new_handle.y - fixed_point.y)
-	)
-
-	var snapped_handle := fixed_point + direction * new_size
-
 	if centered:
-		selected.shape.size = new_size
+		selected.size = new_size
 		selected.position = drag_start_position
 	else:
-		var new_center := (fixed_point + snapped_handle) / 2.0
+		var new_center := Vector2.ZERO
 
-		selected.shape.size = new_size
+		match drag_handle:
+			0, 1, 2, 3:
+				var direction := Vector2(
+					sign(new_handle.x - fixed_point.x),
+					sign(new_handle.y - fixed_point.y)
+				)
+
+				var snapped_handle := fixed_point + direction * new_size
+				new_center = (fixed_point + snapped_handle) / 2.0
+
+			4:
+				new_center.y = (drag_start_size.y - new_size.y) / 2.0
+
+			5:
+				new_center.x = (new_size.x - drag_start_size.x) / 2.0
+
+			6:
+				new_center.y = (new_size.y - drag_start_size.y) / 2.0
+
+			7:
+				new_center.x = (drag_start_size.x - new_size.x) / 2.0
+
+		selected.size = new_size
 		selected.position = drag_start_position + new_center
 
 	update_overlays()
